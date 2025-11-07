717b79d69f31aad14137f95ab262fb22da6d6b4f
Russell Chen <russellchen1234@gmail.com>
2025-11-07 14:50:17 -0600

    shot dist done

--- player_reports/shot_dist/get_df.py
+++ player_reports/shot_dist/get_df.py
@@ -1,5 +1,6 @@
 """ Functions which generate the shot distribution part of the mbb scouting report """
 import pandas as pd
+import numpy as np
 
 def get_shot_dist_df(df : pd.DataFrame, team_id : int) -> pd.DataFrame:
     """
@@ -39,4 +40,36 @@ def get_shot_dist_df(df : pd.DataFrame, team_id : int) -> pd.DataFrame:
         pd.Dataframe
             A dataframe describing the shot distribution chart
     """
-    return pd.DataFrame()
+    opp = df.loc[df["teamId"] == team_id].copy()
+
+    opp["3s Rate"] = np.where(
+        (opp["fga"] > 0) & (opp["fga3"] > 0),  # require some 3PA
+        opp["fga3"]/opp["fga"],
+        np.nan,
+    )
+
+    opp["3s%"] = np.where(
+        opp["fga3"] > 0,
+        opp["fgm3"]/opp["fga3"],
+        np.nan,
+    )
+
+    opp["Rim Rate"] = np.nan
+    opp["Rim%"] = np.nan
+    opp["Mid-Range Rate"] = np.nan
+    opp["Mid-Range FG%"] = np.nan
+
+    output = pd.DataFrame({
+        "Number/Player": "#" + opp["jerseyNum"].astype(str) + " " + opp["fullName"].astype(str),
+        "FGA/G": opp["fgaPg"],
+        "FT Rate": opp["ftARate"],
+        "FT%": opp["ftPct"],
+        "Rim Rate": opp["Rim Rate"],
+        "Rim%": opp["Rim%"],
+        "3s Rate": opp["3s Rate"],
+        "3s%": opp["3s%"],
+        "Mid-Range Rate": opp["Mid-Range Rate"],
+        "Mid-Range FG%": opp["Mid-Range FG%"],
+    })
+
+    return output.fillna("N/A").reset_index(drop=True)
